14c93b1d65dca47911808c3cbfc280f5135f0316
Gourav Agarwal <agarwal.gourav@cognizant.com>
2016-06-01 15:13:27 +0800

    Added react-hot-loader for live reloading

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,28 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
-  }
+  	plugins: [
+		  new webpack.HotModuleReplacementPlugin()
+	],
+  	entry: [
+  		'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+  		'webpack/hot/only-dev-server',
+  		APP_DIR + '/index'
+	],
+  	output: {
+    	path: BUILD_DIR,
+		filename: 'bundle.js',
+		publicPath: '/static/'
+  	},
+  	module : {
+    	loaders : [
+      		{
+		        test : /\.js$/,
+		        include : APP_DIR,
+		        loaders: ['react-hot', 'babel']
+      		}
+    	]
+  	}
 };
 
 module.exports = config;
